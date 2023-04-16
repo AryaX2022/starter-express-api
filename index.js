08ff61c6e8b65fff713474ba51894c19fa6203e1
AryaX2022 <124493388+AryaX2022@users.noreply.github.com>
2023-04-16 12:47:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,131 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+var fs = require('fs');
+const express = require('express');
+const app = express();
+const https = require('https');
+
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./aihub-992d3-4384543c202c.json');
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://prereleasedmodel123.web.app'
+}));
+
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+const { collection, addDoc, getDocs, setDoc, doc, updateDoc, increment, Timestamp, serverTimestamp  } = require('firebase-admin/firestore');
+
+app.get('/transfer', (req, res) => {
+  let url = req.query.url;
+  console.log(url);
+  //res.send(`<h1>${req.params.id}</h1>`);
+  //res.end();
+
+  https.get(url, (response) => {
+    console.log(response);
+    console.log("获取location:");
+    console.log(response.headers["location"]);
+    res.json({target:response.headers["location"]});
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+
+});
+
+function compare( a, b ) {
+    if ( a.stats.ratingCount < b.stats.ratingCount ){
+        return 1;
+    }
+    if ( a.stats.ratingCount > b.stats.ratingCount ){
+        return -1;
+    }
+    return 0;
+}
+
+app.get('/productsPart', async function(req, res) {
+
+    let modelsRef = await db.collection('AIModels')
+        const models = await modelsRef.select('id','name').get();
+        console.log(models);
+    var products = []
+    models.forEach((doc) => {
+        products.push(doc.data());
+                //如果nsfw，则不返回图片地址
+    });
+    //return products;
+    res.json({items:products});
+
+});
+
+app.get('/products', async function(req, res) {
+
+    const models = await db.collection('AIModels').get(); //getDocs(collection(db, "AIModels"));
+    console.log(models);
+    var products = []
+    models.forEach((doc) => {
+        products.push(doc.data());
+                //如果nsfw，则不返回图片地址
+    });
+    products.sort(compare);
+    //return products;
+    res.json({items:products});
+
+});
+
+
+
+app.get('/productsPart', async function(req, res) {
+
+    let modelsRef = await db.collection('AIModels')
+        const models = await modelsRef.select('id','name').get();
+        console.log(models);
+    var products = []
+    models.forEach((doc) => {
+        products.push(doc.data());
+                //如果nsfw，则不返回图片地址
+    });
+    //return products;
+    res.json({items:products});
+
+});
+
+app.get('/products', async function(req, res) {
+
+    const models = await db.collection('AIModels').get(); //getDocs(collection(db, "AIModels"));
+    console.log(models);
+    var products = []
+    models.forEach((doc) => {
+        products.push(doc.data());
+                //如果nsfw，则不返回图片地址
+    });
+    products.sort(compare);
+    //return products;
+    res.json({items:products});
+
+});
+
+app.get('/product/:id', async function(req, res) {
+        console.log(req.params.id);
+        const model = await db.collection('AIModels').doc(req.params.id.toString()).get();
+        //如果nsfw，则不返回图片地址
+        res.json({item:model.data()});
+});
+
+
+
+var privateKey  = fs.readFileSync('./sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('./sslcert/server.cert', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(process.env.PORT || 3001);
+
+console.log(('listening :)'));
